1caf037edc65d267113bea50d62968bc97215ca8
Jan-Jaap Driessen <devnull@localhost>
2011-01-10 16:37:46 +0100

    Fanstatictemplate setup.py - installs a paster template.

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '1.0'
+
+setup(name='fanstatictemplate',
+      version=version,
+      description="Fanstatic package template",
+      author='Fanstatic Developers',
+      author_email='fanstatic@googlegroups.com',
+      url='http://fanstatic.org',
+      license='BSD',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'PasteScript',
+      ],
+      entry_points={
+          'paste.paster_create_template':
+              ['fanstatic = fanstatictemplate:Template']},
+      )
