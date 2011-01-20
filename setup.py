d7ac10633f884845dd96f81e0b17a4f06350360f
Jan-Jaap Driessen <devnull@localhost>
2011-01-20 17:34:14 +0100

    Back to development: 1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import sys
 import os
 from setuptools import setup, find_packages
 
-version = '1.1'
+version = '1.2dev'
 
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
