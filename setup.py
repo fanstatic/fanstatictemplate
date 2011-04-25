c10a75d5c1a04f15b21b062b82de56814386ca97
Jan-Jaap Driessen <devnull@localhost>
2011-04-25 13:14:13 +0200

    Preparing release 1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import sys
 import os
 from setuptools import setup, find_packages
 
-version = '1.2dev'
+version = '1.2'
 
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
