df4ae3ba0ddda3cb0d9174246eb32d7f29ba8039
Jan-Jaap Driessen <devnull@localhost>
2013-04-18 15:36:03 +0200

    Preparing release 1.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import sys
 import os
 from setuptools import setup, find_packages
 
-version = '1.4dev'
+version = '1.4'
 
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
